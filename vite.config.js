8ddf685b4b20c0a560ea4892ef15404f84ff4371
Sai Geethika Akkineni <geethika.akkineni21@gmail.com>
2025-09-03 22:50:19 -0400

    chore: add deploy scripts, vite base config, and gitignore

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/", // 👈 IMPORTANT for GitHub Pages
+  base: "/my-portfolio/", // MUST match your repo name
 });
-
