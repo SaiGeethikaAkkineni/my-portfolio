157f81ed4826bfda91f6d2b63e845f5f97b6cadc
Sai Geethika Akkineni <geethika.akkineni21@gmail.com>
2025-09-08 13:13:14 -0400

    set vite base and ensure index.html uses %BASE_URL% for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/my-portfolio/', // <-- change this to '/<repo-name>/' or '/' if your repo is username.github.io
   plugins: [react()],
-  base: "/my-portfolio/", // 👈 must match your repo name exactly
-});
+})
